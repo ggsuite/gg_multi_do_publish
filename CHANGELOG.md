8b9029e7fa95fe720033d318e8e27961fba5af9c
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2026-08-10 16:28:34 +0200

    Various fixes
    * #gg: changed references to path
    
    * Various log and color fixes across the gg command output
    
    * #gg: Add .gg/gg.json check results
    
    * #gg: Add .gg/gg.json check results
    
    * Various fixes
    
    * #gg: Add .gg/gg.json check results
    
    * #gg: changed references to pub.dev
    
    * #gg: Finish development of version 2.4.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## 2.4.2 - 2026-08-10
+
+### Fixed
+
+- Various log and color fixes across the gg command output
+- Various fixes
+
 ## 2.4.1 - 2026-08-10
 
 ### Fixed
--- lib/src/gg_multi_do_publish_version.dart
+++ lib/src/gg_multi_do_publish_version.dart
@@ -9,4 +9,4 @@
 // coverage:ignore-file
 
 /// The version of the `gg_multi_do_publish` package.
-const String ggMultiDoPublishVersion = '2.4.1';
+const String ggMultiDoPublishVersion = '2.4.2';
